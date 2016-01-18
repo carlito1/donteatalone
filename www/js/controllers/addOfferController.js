ad92fbc89f9156fb4f1f3f2d62aa95bd3d9d847e
mdezelak <mdezelak15@gmail.com>
2016-01-18 16:49:11 +0100

    test commit

--- www/js/controllers/addOfferController.js
+++ www/js/controllers/addOfferController.js
@@ -7,7 +7,7 @@ angular.module('sentdevs.controllers.addOffer', [])
         console.log($scope.offer);
         offersService.createOffer($scope.offer).then(function(bStatus) {
             console.log('Totrata', bStatus);
-            $state.go('loged.tab.trending');
+            $state.go('loged.tab.trending');  
         });
     };
 }]);
